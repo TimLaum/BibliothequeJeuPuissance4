1e75ed13f599b3726f122cd2fe7048a8dd6f74b9
Timothée <timothee.laumonier@gmail.com>
2023-05-20 15:53:19 +0200

    Modification des classes et rajouts pour tester le bon fonctionnement de toutes les valeurs, avec des fonctions de renvois de donées et des variables privées non modifiables pour éviter de trop modifier les valeurs privées dont on se servira pour a suite

--- BibliothequeJeuPuissance4/Partie.cs
+++ BibliothequeJeuPuissance4/Partie.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BibliothequeJeuPuissance4
 {
-	public class Partie { 
-		private int lignes,colonnes;
-		private int[,] plateau;
-		private bool JoueurActif;
+	public class Partie
+	{
+		private int lignes {get;}
+		private int colonnes {get;}
+		private int[,] plateau { get; set; }
+        private bool JoueurActif { get; }
 
+		public void InitPlateau()
+		{
+			plateau = new int[lignes, colonnes];
+		}
+		public Partie(int l,int c,bool JA) 
+		{
+			lignes = l;
+			colonnes = c;
+			JoueurActif = JA;
+			InitPlateau();
+		}
 
-		
+		public void GetTaillePlateau()
+		{
+            Console.WriteLine(Convert.ToString(plateau.GetLength(0))); 
+            Console.WriteLine(Convert.ToString(plateau.GetLength(1)));
+        }
 
+    }
 }
--- BibliothequeJeuPuissance4/Pion.cs
+++ BibliothequeJeuPuissance4/Pion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BibliothequeJeuPuissance4;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,35 +9,60 @@ namespace BibliothequeJeuPuissance4
 {
     public class Pion
     {
-        private int N_Pion;
-        private string couleur;
-        public int NumP;
-        public string PionC;
-        public Pion(int n, string s)
+        private int N_Pion { get;}
+        private string couleur { get;}
+        public int _N_Pion;
+        public string _couleur;
+        public int GetN_Pion()
         {
-            NumP = n;
-            PionC = s;
-             
-            if(PionC == "Rouge" || PionC=="Jaune" || PionC == "Noir" || PionC == "Blanc")
-            {
-                return;
-            }
+            return N_Pion;
         }
 
+        public string GetCouleur()
+        {
+            return couleur;
+        }
 
-
+    public Pion(int nPion, string color)
+    {
+        _N_Pion = nPion;
+        _couleur = color;
+        //Permet d'avoir des valeurs publiques pour vérifier leurs validité avant d'inserer les données dans les variables privées
+        //Ici il n'y a encore aucune vérification pour l'instant
+        N_Pion = _N_Pion;
+        couleur = _couleur;
+        }
     }
+
+
     public class Joueur : Pion
     {
-        private int N_Joueur;
-        private string PseudoJoueur;
-        public Joueur(int nJ,string PionC, string s) : base(nJ,PionC)
+        private int N_Joueur { get; } //Get pour dire qu'il est en lecture uniquement, a part lors de la construction, N_Joueur ne peut pas
+        //Voir sa valeur changer
+        private string PseudoJoueur { get;} 
+        public int _N_Joueur;
+        public string _PseudoJoueur;
+
+        public Joueur(int nJ, string PionC, string s) : base(nJ, PionC)
         {
-            N_Joueur = nJ;
-            PseudoJoueur = s;
+            _N_Joueur = nJ;
+            _PseudoJoueur = s;
+            //Permet d'avoir des valeurs publiques pour vérifier leurs validité avant d'inserer les données dans les variables privées
+            //Ici il n'y a encore aucune vérification pour l'instant
+            N_Joueur = _N_Joueur;
+            PseudoJoueur = _PseudoJoueur;
         }
 
-        
+        public int GetN_Joueur()
+        {
+            return N_Joueur;
+        }
+
+        public string GetPseudoJoueur()
+        {
+            return PseudoJoueur;
+        }
     }
 
 }
+

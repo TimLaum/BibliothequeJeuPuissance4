cba688c7b24a7ac73b35c8ef9b08bc9bc7526991
Claire <claire.heuv@gmail.com>
2023-05-23 14:19:39 +0200

    Renommage des WindowsForm avec nom explicite

--- SAE - Puissance 4/Program.cs
+++ SAE - Puissance 4/Program.cs
@@ -11,7 +11,7 @@ namespace SAE___Puissance_4
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new FormParametrage());
         }
     }
 }

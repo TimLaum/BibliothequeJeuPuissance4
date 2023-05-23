5ccace81bc0718f37cb61165f473ceae4fbbcca4
Claire <claire.heuv@gmail.com>
2023-05-23 14:14:36 +0200

    Page d'accueil + Partie personnalisée AVEC IA

---
+++ SAE - Puissance 4/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SAE___Puissance_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            btnQuitter = new Button();
+            btnAPropos = new Button();
+            btPartPerso = new Button();
+            btn2J = new Button();
+            btn1J = new Button();
+            lblTitre = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnQuitter);
+            panel1.Controls.Add(btnAPropos);
+            panel1.Controls.Add(btPartPerso);
+            panel1.Controls.Add(btn2J);
+            panel1.Controls.Add(btn1J);
+            panel1.Location = new Point(340, 121);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(639, 629);
+            panel1.TabIndex = 3;
+            // 
+            // btnQuitter
+            // 
+            btnQuitter.Location = new Point(222, 503);
+            btnQuitter.Name = "btnQuitter";
+            btnQuitter.Size = new Size(183, 55);
+            btnQuitter.TabIndex = 4;
+            btnQuitter.Text = "Quitter";
+            btnQuitter.UseVisualStyleBackColor = true;
+            // 
+            // btnAPropos
+            // 
+            btnAPropos.Location = new Point(120, 360);
+            btnAPropos.Name = "btnAPropos";
+            btnAPropos.Size = new Size(386, 54);
+            btnAPropos.TabIndex = 3;
+            btnAPropos.Text = "A Propos";
+            btnAPropos.UseVisualStyleBackColor = true;
+            // 
+            // btPartPerso
+            // 
+            btPartPerso.Location = new Point(120, 260);
+            btPartPerso.Name = "btPartPerso";
+            btPartPerso.Size = new Size(386, 54);
+            btPartPerso.TabIndex = 2;
+            btPartPerso.Text = "Partie Personnalisée";
+            btPartPerso.UseVisualStyleBackColor = true;
+            // 
+            // btn2J
+            // 
+            btn2J.Location = new Point(120, 160);
+            btn2J.Name = "btn2J";
+            btn2J.Size = new Size(386, 54);
+            btn2J.TabIndex = 1;
+            btn2J.Text = "2 Joueurs";
+            btn2J.UseVisualStyleBackColor = true;
+            // 
+            // btn1J
+            // 
+            btn1J.Location = new Point(120, 60);
+            btn1J.Name = "btn1J";
+            btn1J.Size = new Size(386, 54);
+            btn1J.TabIndex = 0;
+            btn1J.Text = "1 Joueur";
+            btn1J.UseVisualStyleBackColor = true;
+            // 
+            // lblTitre
+            // 
+            lblTitre.AutoSize = true;
+            lblTitre.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTitre.Location = new Point(516, 15);
+            lblTitre.Name = "lblTitre";
+            lblTitre.Size = new Size(280, 62);
+            lblTitre.TabIndex = 2;
+            lblTitre.Text = "Puissance 4";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1316, 813);
+            Controls.Add(panel1);
+            Controls.Add(lblTitre);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnQuitter;
+        private Button btnAPropos;
+        private Button btPartPerso;
+        private Button btn2J;
+        private Button btn1J;
+        private Label lblTitre;
+    }
+}
---
+++ SAE - Puissance 4/Form2.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace SAE___Puissance_4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            radioButton11 = new RadioButton();
+            radioButton10 = new RadioButton();
+            radioButton9 = new RadioButton();
+            label5 = new Label();
+            lblPseudos = new Label();
+            pnlPseudos = new Panel();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            lblJ2 = new Label();
+            lblJ1 = new Label();
+            panel3 = new Panel();
+            radioButton6 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            panel2 = new Panel();
+            radioButton8 = new RadioButton();
+            radioButton7 = new RadioButton();
+            panel1 = new Panel();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            pnlPseudos.SuspendLayout();
+            panel3.SuspendLayout();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(548, 571);
+            button1.Name = "button1";
+            button1.Size = new Size(269, 57);
+            button1.TabIndex = 26;
+            button1.Text = "JOUER !";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            radioButton11.AutoSize = true;
+            radioButton11.Location = new Point(895, 508);
+            radioButton11.Name = "radioButton11";
+            radioButton11.Size = new Size(91, 24);
+            radioButton11.TabIndex = 25;
+            radioButton11.TabStop = true;
+            radioButton11.Text = "Aléatoire";
+            radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            radioButton10.AutoSize = true;
+            radioButton10.Location = new Point(631, 508);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(124, 24);
+            radioButton10.TabIndex = 24;
+            radioButton10.TabStop = true;
+            radioButton10.Text = "Joueur 2 ou IA";
+            radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            radioButton9.AutoSize = true;
+            radioButton9.Location = new Point(411, 508);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(85, 24);
+            radioButton9.TabIndex = 23;
+            radioButton9.TabStop = true;
+            radioButton9.Text = "Joueur 1";
+            radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(544, 446);
+            label5.Name = "label5";
+            label5.Size = new Size(245, 20);
+            label5.TabIndex = 22;
+            label5.Text = "Choix du joueur débutant la partie";
+            // 
+            // lblPseudos
+            // 
+            lblPseudos.AutoSize = true;
+            lblPseudos.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPseudos.Location = new Point(277, 264);
+            lblPseudos.Name = "lblPseudos";
+            lblPseudos.Size = new Size(124, 38);
+            lblPseudos.TabIndex = 21;
+            lblPseudos.Text = "Pseudos";
+            // 
+            // pnlPseudos
+            // 
+            pnlPseudos.Controls.Add(textBox2);
+            pnlPseudos.Controls.Add(textBox1);
+            pnlPseudos.Controls.Add(lblJ2);
+            pnlPseudos.Controls.Add(lblJ1);
+            pnlPseudos.Location = new Point(199, 288);
+            pnlPseudos.Name = "pnlPseudos";
+            pnlPseudos.Size = new Size(926, 144);
+            pnlPseudos.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(344, 80);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(325, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(344, 24);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(325, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // lblJ2
+            // 
+            lblJ2.AutoSize = true;
+            lblJ2.Location = new Point(138, 87);
+            lblJ2.Name = "lblJ2";
+            lblJ2.Size = new Size(64, 20);
+            lblJ2.TabIndex = 1;
+            lblJ2.Text = "Joueur 2";
+            // 
+            // lblJ1
+            // 
+            lblJ1.AutoSize = true;
+            lblJ1.Location = new Point(138, 31);
+            lblJ1.Name = "lblJ1";
+            lblJ1.Size = new Size(64, 20);
+            lblJ1.TabIndex = 0;
+            lblJ1.Text = "Joueur 1";
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(radioButton6);
+            panel3.Controls.Add(radioButton5);
+            panel3.Controls.Add(radioButton4);
+            panel3.Location = new Point(544, 161);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(487, 42);
+            panel3.TabIndex = 18;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(356, 10);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(116, 24);
+            radioButton6.TabIndex = 2;
+            radioButton6.Text = "Rouge / Noir";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(200, 10);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(109, 24);
+            radioButton5.TabIndex = 1;
+            radioButton5.Text = "Blanc / Noir";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Checked = true;
+            radioButton4.Location = new Point(54, 10);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(124, 24);
+            radioButton4.TabIndex = 0;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Rouge / Jaune";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(radioButton8);
+            panel2.Controls.Add(radioButton7);
+            panel2.Location = new Point(544, 209);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(487, 42);
+            panel2.TabIndex = 19;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(200, 10);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(73, 24);
+            radioButton8.TabIndex = 1;
+            radioButton8.Text = "Joueur";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Checked = true;
+            radioButton7.Location = new Point(54, 10);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(44, 24);
+            radioButton7.TabIndex = 0;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "IA";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(544, 113);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(487, 42);
+            panel1.TabIndex = 17;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(356, 8);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(61, 24);
+            radioButton3.TabIndex = 2;
+            radioButton3.Text = "9 x 9";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(200, 8);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(61, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.Text = "8 x 8";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(54, 8);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(61, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "6 x 7";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(314, 223);
+            label4.Name = "label4";
+            label4.Size = new Size(146, 20);
+            label4.TabIndex = 16;
+            label4.Text = "Choix de l'adversaire";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(314, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(127, 20);
+            label3.TabIndex = 15;
+            label3.Text = "Couleur des pions";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(314, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 20);
+            label2.TabIndex = 14;
+            label2.Text = "Taille du plateau";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(411, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(469, 62);
+            label1.TabIndex = 13;
+            label1.Text = "Partie Personnalisée";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1345, 646);
+            Controls.Add(button1);
+            Controls.Add(radioButton11);
+            Controls.Add(radioButton10);
+            Controls.Add(radioButton9);
+            Controls.Add(label5);
+            Controls.Add(lblPseudos);
+            Controls.Add(pnlPseudos);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            pnlPseudos.ResumeLayout(false);
+            pnlPseudos.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private RadioButton radioButton11;
+        private RadioButton radioButton10;
+        private RadioButton radioButton9;
+        private Label label5;
+        private Label lblPseudos;
+        private Panel pnlPseudos;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label lblJ2;
+        private Label lblJ1;
+        private Panel panel3;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private Panel panel2;
+        private RadioButton radioButton8;
+        private RadioButton radioButton7;
+        private Panel panel1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
